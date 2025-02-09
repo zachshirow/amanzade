62756a56026d84e0ab75dfabbe3d11984c760e78
Zachary Shirmohammadli <zachshirow@gmail.com>
2025-02-09 09:44:23 +0330

    first commit from amanzade

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+
+export const site = {
+    name: "یوسف امان زاده سید",
+    description: "ترخیص کار در گمرک های شمال ایران"
+}
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,28 @@ import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
+import IconInstagram from "../components/icons/instagram.astro"
+
 
 export const SOCIALS = [
   {
-    name: "Github",
-    url: "https://github.com/thevrus",
-    icon: IconGithub,
+    name: "Instagram",
+    url: "https://instagram.com/yousef_amanzadeh91",
+    icon: IconInstagram,
   },
-  {
-    name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
-    icon: IconLinkedin,
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
-    icon: IconTwitter,
-  },
-  {
-    name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
-    icon: IconDribbble,
-  },
+  // {
+  //   name: "Linkedin",
+  //   url: "https://www.linkedin.com/in/vrusin/",
+  //   icon: IconLinkedin,
+  // },
+  // {
+  //   name: "Twitter",
+  //   url: "https://twitter.com/rusin_vadim",
+  //   icon: IconTwitter,
+  // },
+  // {
+  //   name: "Dribbble",
+  //   url: "https://dribbble.com/iamvr",
+  //   icon: IconDribbble,
+  // },
 ];
